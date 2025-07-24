98c288b9563d22ff601d43ac209643e2bafa0bb6
giobbu <giovanniburoni@gmail.com>
2025-07-24 17:55:23 +0200

    update notebook PCA visualization

--- setting/rag/embedding/pca_vis_config.py
+++ setting/rag/embedding/pca_vis_config.py
@@ -1,7 +1,7 @@
 class PCAConfig:
     top_k = 3
-    dataset_name = "QA/gpt-35-turbo_dataset.json"
+    dataset_name = "QA/codice/gpt-35-turbo_temperature_1_dataset.json"
     model_name = "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
     num_pca_components = 2
     num_epochs = 2
-    finetuned_filename = "finetuned-sentence-transformers/finetuned-paraphrase-multilingual-MiniLM-L12-v2"
+    finetuned_filename = "finetuned-sentence-transformers/codice/finetuned-paraphrase-multilingual-MiniLM-L12-v2_gpt-35-turbo_temperature_1_dataset"
